f94d5d5f5d91af96793639c7523f084f48162af4
Aziz Woubi <azizwoubi@gmail.com>
2025-12-09 16:52:08 +0000

    Mise à jour 6 du site ARCOP

--- venvarc3/Scripts/activate.bat
+++ venvarc3/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\azizn\Downloads\arcop_website_v2\arcop_website\venvarc2
+set VIRTUAL_ENV=C:\Users\azizn\Downloads\arcop_website_v2\arcop_website\venvarc3
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -16,7 +16,7 @@ if defined _OLD_VIRTUAL_PROMPT set PROMPT=%_OLD_VIRTUAL_PROMPT%
 if defined _OLD_VIRTUAL_PYTHONHOME set PYTHONHOME=%_OLD_VIRTUAL_PYTHONHOME%
 
 set _OLD_VIRTUAL_PROMPT=%PROMPT%
-set PROMPT=(venvarc2) %PROMPT%
+set PROMPT=(venvarc3) %PROMPT%
 
 if defined PYTHONHOME set _OLD_VIRTUAL_PYTHONHOME=%PYTHONHOME%
 set PYTHONHOME=
@@ -25,7 +25,7 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(venvarc2) 
+set VIRTUAL_ENV_PROMPT=(venvarc3) 
 
 :END
 if defined _OLD_CODEPAGE (
